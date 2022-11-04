87c6fc0c186fe6b6e894d82b99796dab817cb1eb
Honza Duda <doodah@post.cz>
2022-11-04 08:47:08 +0100

    initial commit

---
+++ src/test/java/com/company/durblam/DurblamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.durblam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DurblamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
